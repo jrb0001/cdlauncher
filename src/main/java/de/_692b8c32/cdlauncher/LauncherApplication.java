70574cd6ee4d42b74ea2b8a42726c0c35cf78d79
Jean-Rémy Buchs <jrb0001@692b8c32.de>
2017-03-25 01:53:49 +0100

    - Added main method for native wrappers

--- src/main/java/de/_692b8c32/cdlauncher/LauncherApplication.java
+++ src/main/java/de/_692b8c32/cdlauncher/LauncherApplication.java
@@ -140,4 +140,8 @@ public class LauncherApplication extends Application {
             }
         }
     }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
